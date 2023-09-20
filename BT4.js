2e8aeeb1ae209cc53cc22506297f9bc62b49d885
JuanTRD <tranhoan10091995@gmail.com>
2023-09-20 20:18:12 +0700

    update

---
+++ BT4.js
@@ -0,0 +1,12 @@
+
+    let width=20;
+    let height=10;
+    let area=width*height;
+    let parameters=width+height;
+    document.write("Chiều rộng là: "+ width);
+    document.write("<br>");
+    document.write("Chiều cao là: "+ height);
+    document.write("<br>");
+        document.write("Diện tích là: "+ area);
+        document.write("<br>");
+            document.write("Chu vi là: "+ parameters);
